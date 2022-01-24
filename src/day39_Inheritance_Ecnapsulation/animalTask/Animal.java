e2ec8b303f06851004fccd675e88200f8afed8f4
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2022-01-24 15:09:34 -0500

    Java Practice Tasks

---
+++ src/day39_Inheritance_Ecnapsulation/animalTask/Animal.java
@@ -0,0 +1,96 @@
+package day39_Inheritance_Ecnapsulation.animalTask;
+
+public class Animal {
+
+    private String name, breed;
+    private int age;
+    private char gender;
+    private String size, color;
+
+    public Animal(String name, String breed, int age, char gender, String size, String color) {
+        setName(name);
+        setBreed(breed);
+        setAge(age);
+        setGender(gender);
+        setSize(size);
+        setColor(color);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void eat(){
+        System.out.println("");
+    }
+
+    public void drink(){
+        System.out.println(name+" drinks water");
+    }
+
+    public void sleep(){
+        System.out.println(name+" is sleeping");
+    }
+
+    public void move(){
+        System.out.println(name+" is moving");
+    }
+
+
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", breed='" + breed + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                ", size='" + size + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+
+
+}
